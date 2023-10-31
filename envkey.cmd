7ba147114674b0cc9b26e8f52e1b7bfe19a73ede
Rahmanism <s.mostafa.rahmani@gmail.com>
2023-10-31 07:44:55 +0330

    Add my own prompt and comment out the original one

--- envkey.cmd
+++ envkey.cmd
@@ -3,6 +3,7 @@
 :: Create an alias for cd and git to try to get the branch of the
 :: current project in case of the user is in a git repo 
 DOSKEY cd=cd $* ^&^& %~dp0\winrc.cmd
+:: DOSKEY g=git status ^&^& %~dp0\winrc.cmd
 DOSKEY git=git $* ^&^& %~dp0\winrc.cmd
 
 :: Aliases for virtual envs
--- winrc.cmd
+++ winrc.cmd
@@ -9,36 +9,40 @@ REM to make the script work
 FOR /f "tokens=2" %%a in ('git branch 2^> NUL ^| FINDSTR /b "* "') do SET GIT_BRANCH=%%a
 
 :: Detect if the user is currently using anu virtual env
-SET CURRENT_ENV=%CONDA_DEFAULT_ENV%
-IF [%GIT_BRANCH%] == [] ( GOTO :prompt ) ELSE ( GOTO :custom )
+@REM SET CURRENT_ENV=%CONDA_DEFAULT_ENV%
+@REM IF [%GIT_BRANCH%] == [] ( GOTO :prompt ) ELSE ( GOTO :custom )
 
-:custom
-    IF [%CURRENT_ENV%] == [] (
-        SET CUSTOM_PROMPT=^
-$E[m$E[32m$E]9;8;"USERNAME"$E\@$E]9;8;"COMPUTERNAME"$E\$S$E[92m$P$E[90m^
-$S$E[1;33m$C%GIT_BRANCH%$F$E$_^
-$E[90m$G$E[m$S$E]9;12$E\
-    ) ELSE (
-        SET CUSTOM_PROMPT=^
-$E[35m$C%CURRENT_ENV%$F$S^
-$E[m$E[32m$E]9;8;"USERNAME"$E\@$E]9;8;"COMPUTERNAME"$E\$S$E[92m$P$E[90m^
-$S$E[1;33m$C%GIT_BRANCH%$F$E$_^
-$E[90m$G$E[m$S$E]9;12$E\
-    )
-    prompt %CUSTOM_PROMPT%
-    GOTO :eof
+@REM :custom
+@REM     IF [%CURRENT_ENV%] == [] (
+@REM         SET CUSTOM_PROMPT=^
+@REM $E[m$E[32m$E]9;8;%USERNAME%$E\@$E]9;8;%COMPUTERNAME%$E\$S$E[92m$P$E[90m^
+@REM $S$E[1;33m$C%GIT_BRANCH%$F$E$_^
+@REM $E[90m$G$E[m$S$E]9;12$E\
+@REM     ) ELSE (
+@REM         SET CUSTOM_PROMPT=^
+@REM $E[35m$C%CURRENT_ENV%$F$S^
+@REM $E[m$E[32m$E]9;8;%USERNAME%$E\@$E]9;8;%COMPUTERNAME%$E\$S$E[92m$P$E[90m^
+@REM $S$E[1;33m$C%GIT_BRANCH%$F$E$_^
+@REM $E[90m$G$E[m$S$E]9;12$E\
+@REM     )
+@REM     prompt %CUSTOM_PROMPT%
+@REM     GOTO :eof
 
-:prompt
-    IF [%CURRENT_ENV%] == [] (
-        SET CUSTOM_PROMPT=^
-$E[m$E[32m$E]9;8;"USERNAME"$E\@$E]9;8;"COMPUTERNAME"$E\$S$E[92m$P$E[90m$_^
-$E[90m$G$E[m$S$E]9;12$E\
-        prompt %CUSTOM_PROMPT%
-    ) ELSE (
-        SET CUSTOM_PROMPT=^
-$E[35m$C%CURRENT_ENV%$F$S^
-$E[m$E[32m$E]9;8;"USERNAME"$E\@$E]9;8;"COMPUTERNAME"$E\$S$E[92m$P$E[90m$_^
-$E[90m$G$E[m$S$E]9;12$E\
-    )
-    prompt %CUSTOM_PROMPT%
-    GOTO :eof
+@REM :prompt
+@REM     IF [%CURRENT_ENV%] == [] (
+@REM         SET CUSTOM_PROMPT=^
+@REM $E[m$E[32m$E]9;8;%USERNAME%$E\@$E]9;8;%COMPUTERNAME%$E\$S$E[92m$P$E[90m$_^
+@REM $E[90m$G$E[m$S$E]9;12$E\
+@REM         prompt %CUSTOM_PROMPT%
+@REM     ) ELSE (
+@REM         SET CUSTOM_PROMPT=^
+@REM $E[35m$C%CURRENT_ENV%$F$S^
+@REM $E[m$E[32m$E]9;8;%USERNAME%$E\@$E]9;8;%COMPUTERNAME%$E\$S$E[92m$P$E[90m$_^
+@REM $E[90m$G$E[m$S$E]9;12$E\
+@REM     )
+@REM     prompt %CUSTOM_PROMPT%
+@REM     GOTO :eof
+
+prompt $_$E[1;30;40m[$E[0;36;40m%username%@%computername%$E[1;30;40m] ^
+$E[0;33;40m$M$E[35;40m$d$s$t$h$h$h$h$h$h$_$E[1;33;40m$p$E$S$E[1;92m ^
+$C%GIT_BRANCH%$F$E[1;30;40m$g $E[0;37;40m
